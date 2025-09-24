d1bb1cc5874cf14567748f8bc0f3f2e7dc924b06
CynthiaKaluson <cynthiakalu1974@gmail.com>
2025-09-24 04:45:41 +0200

    new update on index.tsx

--- alx-project-0x00/pages/index.ts
+++
@@ -1,7 +0,0 @@
-export interface ButtonProps {
-    title: string;
-    size?: 'small' | 'medium' | 'large';
-    shape?: 'rounded-sm' | 'rounded-md' | 'rounded-full';
-    className?: string;
-    styles?: string;
-}
--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,12 +1,7 @@
-import Button from "@/components/Button";
-
-const Home: React.FC = () => {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <Button title="Click Me" size="medium" shape="rounded-md" />
-    </div>
-  );
-};
-
-export default Home;
+export interface ButtonProps {
+  title: string;
+  size?: 'small' | 'medium' | 'large';
+  shape?: 'rounded-sm' | 'rounded-md' | 'rounded-full';
+  className?: string;
+  styles?: string;
+}
